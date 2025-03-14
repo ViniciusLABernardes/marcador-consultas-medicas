7ac7ec506f5d9d2f9c6647cdc0f393ed1b9e9a63
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2025-03-13 22:22:31 -0300

    Finalizando componente HomeScreen e configurando App.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import { SafeAreaView,StatusBar } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text >Roberto</Text>
-    
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaView style={{flex:1}}>
+        <StatusBar barStyle={"light-content" } backgroundColor="#007BFF"/>
+
+        <Stack.Navigator>
+          
+          <Stack.Screen name='Home'
+          component={HomeScreen}
+          options={{headerShown:false}}
+          ></Stack.Screen>
+
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
